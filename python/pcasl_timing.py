365adde9ac7a85f1ad25d149c21c597e08fb1c8e
Haley Acevedo <haley.acevedo2@gmail.com>
2017-12-20 14:23:11 -0600

    added header rows - fixed scan number for start times table - added command arg for rerunning even if file exists

--- python/pcasl_timing.py
+++ python/pcasl_timing.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import dicom
 import os
@@ -12,9 +13,10 @@ SCAN_START_FILE = 'scan_start_times.csv'
 CBF_PAIR_FILE = 'cbf_pair_times.csv'
 
 
-def write_csv(rows, outfile):
+def write_csv(header, rows, outfile):
 	with open(outfile, 'wb') as f:
 		writer = csv.writer(f)
+		writer.writerow(header)
 		for row in rows:
 			writer.writerow(row)
 
@@ -29,8 +31,8 @@ def read_csv(infile):
 
 
 # for all dicom images in a study 99 folder, get list of tuples containing series number and content time
-def get_content_time_table(patid):
-	if exists(SCAN_START_FILE):
+def get_content_time_table(patid, rerun=False):
+	if not rerun and exists(SCAN_START_FILE):
 		return read_csv(SCAN_START_FILE)
 
 	os.chdir('study99')
@@ -43,20 +45,21 @@ def get_content_time_table(patid):
 		# chose to just convert header to string for ease of checking
 		# relies on assumption that search string should never appear in other types of scans)
 		if 'pcasl_3D_tgse_3mm_SEG2by2' in str(ds):
-			content_times.append((patid, ds.InstanceNumber, ds.ContentTime))
+			content_times.append((patid, ds.InstanceNumber, ds.ContentTime)) # instance number here is number of scan out of all scans
 
-	write_csv(sorted(content_times), join('..', SCAN_START_FILE))
+	content_times = [ (tup[0], idx+1, tup[2]) for idx, tup in enumerate(sorted(content_times)) ] # renumber for asl scans only
+	write_csv(['patid', 'asl_run', 'start_time'], sorted(content_times), join('..', SCAN_START_FILE))
 	return content_times
 
 
-def get_cbf_time_table(patient_dir):
+def get_cbf_time_table(patient_dir, rerun=False):
 	os.chdir(patient_dir)
 
-	if exists(CBF_PAIR_FILE):
+	if not rerun and exists(CBF_PAIR_FILE):
 		return read_csv(CBF_PAIR_FILE)
 
 	patid = os.getcwd().strip(os.sep).split(os.sep)[-1] # remove trailing slash, split on file separator, then get patid (current directory name)
-	content_times = get_content_time_table(patid)
+	content_times = get_content_time_table(patid, rerun)
 
 	cbf_pair_times = []
 	for asl_run in range(1, len(content_times) + 1):
@@ -69,9 +72,14 @@ def get_cbf_time_table(patient_dir):
 				pair_time = content_time + timedelta(seconds=((cbf_pair * 2) + 1) * 16.92)
 				cbf_pair_times.append((patid, asl_run, cbf_pair, pair_time.strftime('%H:%M:%S.%f')))
 
-	write_csv(cbf_pair_times, join('..', CBF_PAIR_FILE))
+	write_csv(['patid', 'asl_run', 'cbf_pair', 'time'], cbf_pair_times, join('..', CBF_PAIR_FILE))
 	return cbf_pair_times
 
 
 if __name__ == '__main__':
-	get_cbf_time_table(sys.argv[1])
+	parser = argparse.ArgumentParser(description='generate time tables for scan starts and cbf pairs')
+	parser.add_argument('patdir', help='patient scan session directory to generate time tables for')
+	parser.add_argument('-r', '--rerun', action='store_true', help='regenerate time tables')
+	args = parser.parse_args()
+
+	get_cbf_time_table(args.patdir, args.rerun)
