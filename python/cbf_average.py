a05688a2cdb828aebaf73ca89f3842a88f0cf169
Haley Acevedo <haley.acevedo2@gmail.com>
2019-10-15 10:39:49 -0500

    added support for excluding motion corrupted frames

--- python/cbf_average.py
+++ python/cbf_average.py
@@ -4,12 +4,11 @@ import pcasl_timing
 from datetime import datetime, timedelta
 from os import chdir, getcwd
 from os.path import split
-from subprocess import call
+from subprocess import call, PIPE, run
 
 
-def post_infusion_cbf_avg(cbf_conc_img, output_root_trailer, infusion_time):
+def post_infusion_cbf_avg(cbf_conc_img, output_root_trailer, infusion_time, fmtfile=None):
 	patient_dir, conc_img = split(cbf_conc_img)
-	chdir(patient_dir)
 
 	infusion_time = datetime.strptime(infusion_time, '%H%M%S')
 	cbf_time_table = pcasl_timing.get_cbf_time_table(getcwd(), infusion_time)
@@ -18,8 +17,14 @@ def post_infusion_cbf_avg(cbf_conc_img, output_root_trailer, infusion_time):
 	during = next((idx for idx, tup in enumerate(cbf_time_table[before:]) if float(tup[-1]) >= 40), 0)
 	after = len(cbf_time_table) - before - during
 
-	format_str = ''.join([str(before), 'x', str(during), '+', str(after), 'x'])
-	call(['actmapf_4dfp', format_str, conc_img, '-a' + output_root_trailer])
+	fmt_str = ''.join([str(before), 'x', str(during), '+', str(after), 'x'])
+	if fmtfile:
+		fmt1 = list(run(['format2lst', '-e', fmt_str], stdout=PIPE).stdout.decode())
+		fmt2 = list(open(fmtfile).readlines()[0])
+		print(fmt1)
+		print(fmt2)
+		fmt_str = ''.join([ ('x' if v1 == 'x' else v2) for v1,v2 in zip(fmt1, fmt2) ])
+	call(['actmapf_4dfp', fmt_str, conc_img, '-a' + output_root_trailer])
 
 
 if __name__ == '__main__':
@@ -27,6 +32,7 @@ if __name__ == '__main__':
 	parser.add_argument('cbf_conc_img', help='conc image containing cbf images for all asl runs')
 	parser.add_argument('output_root_trailer', help='trailer to append to output image root (e.g. 15-40min_after_infusion)')
 	parser.add_argument('infusion_time', help='infusion time string in format hhmmss')
+	parser.add_argument('--fmtfile', help='addtional fmtfile (pdvars/fd) to use when creating average')
 	args = parser.parse_args()
 
-	post_infusion_cbf_avg(args.cbf_conc_img, args.output_root_trailer, args.infusion_time)
+	post_infusion_cbf_avg(args.cbf_conc_img, args.output_root_trailer, args.infusion_time, args.fmtfile)
