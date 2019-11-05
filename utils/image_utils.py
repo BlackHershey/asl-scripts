2990d374ba4da294a839376bf94ca7ec033650cf
Haley Acevedo <haley.acevedo2@gmail.com>
2019-11-05 08:57:28 -0600

    fixed typo in ifh check

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -5,6 +5,6 @@ def get_num_frames(ifh):
     header_vars = {}
     with open(ifh) as f:
         for line in f:
-            if line.startswith('matrix size[4]'):
+            if line.startswith('matrix size [4]'):
                 return int(line.split(':=')[-1])
     return 0 # if no 4th dimension in header, not a 4D image
